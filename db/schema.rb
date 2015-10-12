b845fa2901ce68f171d291565abd580eefcf5819
Agnès Serman <agnesserman@outlook.com>
2015-10-12 16:42:11 +0700

    Generate books model and run migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011205951) do
+ActiveRecord::Schema.define(version: 20151012092939) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title",        limit: 255
+    t.integer  "category_id",  limit: 4
+    t.integer  "author_id",    limit: 4
+    t.integer  "publisher_id", limit: 4
+    t.string   "isbn",         limit: 255
+    t.string   "year",         limit: 255
+    t.string   "price",        limit: 255
+    t.string   "buy",          limit: 255
+    t.text     "excerpt",      limit: 65535
+    t.string   "format",       limit: 255
+    t.integer  "pages",        limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
